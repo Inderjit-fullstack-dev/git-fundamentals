24d41cd9ede42df53db83666a15d8ce54c90a107
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-05-05 21:49:32 +0530

    simple.java updated

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple {
 	public static void main(String[] args ) {
-		System.out.println("Hello World");
+		System.out.println("I updated the file to check if the jenkins is updating the build or not.");
 	}
 
 }
